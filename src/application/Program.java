e1d5c21803c49306f79abd084e8d604bc5cc3cc0
Paulo Arthur Simonato <paulo.simonato@hotmail.com>
2019-03-05 17:48:36 -0300

    Chess layer and printing the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,7 @@
  */
 package application;
 
-import boardgame.Position;
+import chess.ChessMatch;
 
 /**
  *
@@ -14,8 +14,9 @@ import boardgame.Position;
 public class Program {
     public static void main(String[] args) {
         
-        Position pos = new Position(3, 5);
-        System.out.println(pos);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
+        
     }
     
     
---
+++ src/chess/ChessPiece.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chess;
+
+import boardgame.Board;
+import boardgame.Piece;
+
+/**
+ *
+ * @author paulo
+ */
+public class ChessPiece extends Piece {
+
+    private Color color;
+
+    public ChessPiece(Board board, Color color) {
+        super(board);
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+}
