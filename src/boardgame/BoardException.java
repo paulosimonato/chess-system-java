c7c74626c49e17524b6c39e8de20dcf5c7826599
Paulo Arthur Simonato <paulo.simonato@hotmail.com>
2019-03-05 20:08:06 -0300

    BoardException and defensive programing

---
+++ src/boardgame/BoardException.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boardgame;
+
+/**
+ *
+ * @author paulo
+ */
+public class BoardException extends RuntimeException {
+    
+    private static final long serialVersionUID = 1L;
+    
+    public BoardException(String msg)
+    {
+        super(msg);
+    }
+}
