54c971676060ebdb53edf537eb92db56a8fae048
Paulo Arthur Simonato <paulo.simonato@hotmail.com>
2019-03-05 18:04:15 -0300

    Placing pieces on the board

---
+++ src/chess/pieces/King.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chess.pieces;
+
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+/**
+ *
+ * @author paulo
+ */
+public class King extends ChessPiece {
+    
+    public King(Board board, Color color) {
+        super(board, color);
+    }
+    
+    @Override
+    public String toString()
+    {
+        return "K";
+    }
+}
