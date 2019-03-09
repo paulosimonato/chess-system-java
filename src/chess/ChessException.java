ab12335e51558e05a0f5eba5d1a4729f4fdf9198
Paulo Arthur Simonato <paulo.simonato@hotmail.com>
2019-03-08 21:29:59 -0300

    Moving pieces

--- src/chess/ChessException.java
+++ src/chess/ChessException.java
@@ -5,11 +5,13 @@
  */
 package chess;
 
+import boardgame.BoardException;
+
 /**
  *
  * @author paulo
  */
-public class ChessException extends RuntimeException{
+public class ChessException extends BoardException{
     
     private static final long serialVersionUID = 1L;
     
